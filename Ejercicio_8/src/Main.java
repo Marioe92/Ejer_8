f3bf401a14f4a5e2b3a844588aebd9c035469ca0
Mario <mario.argote@ikasle.egibide.org>
2024-04-19 21:52:40 +0200

    commit intellij

--- Ejercicio_8/src/Main.java
+++ Ejercicio_8/src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Inmobiliaria inmobiliaria = new Inmobiliaria();
+        Inmuable inmuable = new Inmuable();
+
+        inmobiliaria.addListaInmueble(inmuable);
+        inmuable.setCalle("La paz");
+        inmobiliaria.delListaInmueble(inmuable);
+        System.out.println("fin");
+
     }
 }
