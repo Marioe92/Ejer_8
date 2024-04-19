f4ae0268460c2ec37f0d415eef7db83df1961cbc
Mario <mario.argote@ikasle.egibide.org>
2024-04-19 21:49:41 +0200

    commit intellij

--- Ejercicio_8/src/Inmobiliaria.java
+++ Ejercicio_8/src/Inmobiliaria.java
@@ -1,2 +1,54 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Inmobiliaria {
+
+    private String nombre;
+    private String ubicacion;
+
+    private List<Inmuable> listainmuebles = new ArrayList();
+
+    public Inmobiliaria() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getUbicacion() {
+        return ubicacion;
+    }
+
+    public void setUbicacion(String ubicacion) {
+        this.ubicacion = ubicacion;
+    }
+
+    public List<Inmuable> getListainmuebles() {
+        return listainmuebles;
+    }
+
+    public void setListainmuebles(List<Inmuable> listainmuebles) {
+        this.listainmuebles = listainmuebles;
+    }
+
+    public void addListaInmueble(Inmuable inmuable) {
+        listainmuebles.add(inmuable);
+    }
+
+    public void delListaInmueble(Inmuable inmuable) {
+        listainmuebles.remove(inmuable);
+    }
+
+    @Override
+    public String toString() {
+        return "Inmobiliaria{" +
+                "nombre='" + nombre + '\'' +
+                ", ubicacion='" + ubicacion + '\'' +
+                ", listainmuebles=" + listainmuebles +
+                '}';
+    }
 }
--- Ejercicio_8/src/Inmuable.java
+++ Ejercicio_8/src/Inmuable.java
@@ -1,3 +1,48 @@
 public class Inmuable {
 
+    private String calle;
+    private int metros;
+    private int id;
+
+    public Inmuable() {
+    }
+
+    public Inmuable(String calle, int metros, int id) {
+        this.calle = calle;
+        this.metros = metros;
+        this.id = id;
+    }
+
+    public String getCalle() {
+        return calle;
+    }
+
+    public void setCalle(String calle) {
+        this.calle = calle;
+    }
+
+    public int getMetros() {
+        return metros;
+    }
+
+    public void setMetros(int metros) {
+        this.metros = metros;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Inmuable{" +
+                "calle='" + calle + '\'' +
+                ", metros=" + metros +
+                ", id=" + id +
+                '}';
+    }
 }
